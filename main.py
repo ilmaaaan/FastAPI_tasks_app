620ae819cc9bb099dcd1c096b2d4191db388d231
ilmaaaan <146379146+ilmaaaan@users.noreply.github.com>
2024-06-13 20:54:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+
 from database import create_tables, delete_tables
 
 from router import router as tasks_router
